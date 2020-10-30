36a270e86762ffe0cc98bc05c997685e559dc50a
ogulcansahna <61863684+ogulcansahna@users.noreply.github.com>
2020-10-31 00:49:46 +0300

    deneme

--- app/src/main/java/ise308/sahnaoglu/ogulcan/cardviewproject/MainActivity.kt
+++ app/src/main/java/ise308/sahnaoglu/ogulcan/cardviewproject/MainActivity.kt
@@ -2,11 +2,17 @@ package ise308.sahnaoglu.ogulcan.cardviewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.cardview_content_1.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.mainlayout)
+
     }
+
+
 }
